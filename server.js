0274ac0aacbb086fe3cd7de60321e83a7fb770b8
OlegShlapak00 <oleg.shlapak.00@gmail.com>
2021-02-04 21:28:08 +0200

    v1.1

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(`./build`));
-
-app.get(`/*`, function(req, res) {
-    res.sendFile(`index.html`, {root: `build/`}
-    );
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 
 app.listen(process.env.PORT || 8080);
